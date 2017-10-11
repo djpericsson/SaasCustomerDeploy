2cda9d6d6d42be5725014b93a468136f46268d5e
Peter Ericsson <peter.ericsson@addlevel.se>
2017-10-11 15:13:03 +0200

    --

---
+++ Create-DnsCnameRecord.ps1
@@ -0,0 +1,11 @@
+﻿$DnsRecordsValue = "kdilla.azurewebsites.net"
+$DnsRecords = (New-AzureRmDnsRecordConfig -Cname $DnsRecordsValue)
+$AzureRmDnsRecordSetParams = @{
+    Name                   = "kdilla"
+    RecordType             = "CNAME"
+    ZoneName               = "signupsaasservices.dev.addlevel.net"
+    ResourceGroupName      = "SignUp-SAAS-Services"
+    Ttl                    = "3600"
+    DnsRecords             = $DnsRecords
+}
+New-AzureRmDnsRecordSet @AzureRmDnsRecordSetParams -ErrorAction Stop
